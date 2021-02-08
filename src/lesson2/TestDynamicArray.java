06b66d7eb6bbe2577125cce05bc3c95b3c74a1d1
Andrei <andreyka_ilin@mail.ru>
2021-02-08 08:36:08 +0300

    Initial commit

---
+++ src/lesson2/TestDynamicArray.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+import java.util.ArrayList;
+
+public class TestDynamicArray {
+
+    public static void main(String[] args) {
+        Array<Integer> data = new ArrayImpl<>(4);
+
+    }
+}
